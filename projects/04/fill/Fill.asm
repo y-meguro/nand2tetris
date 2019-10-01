fe33b8aa73117672ba87c95d05884c729f56423d
Yohei Meguro <y.meguro.2008@gmail.com>
2019-10-02 07:20:59 +0900

    solve project04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,82 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    // R0: if keyboard is pressed in previous term then 1
+    // R1: if keyboard is pressed in current term then 1
+
+    @R0
+    M=0 // R0=0
+
+    @8192 // screen size 32*256
+    D=A
+    @number_of_pixel
+    M=D // number_of_pixel=8192
+
+    @SCREEN // 16384
+    D=A
+    @position
+    M=D // position=16384
+    @count
+    M=0 // i=0
+(LOOP_KBD)
+    @KBD
+    D=M // D=KBD
+    @KEY_PRESSED
+    D;JNE
+(KEY_NOT_PRESSED)
+    @R1
+    M=0 // R1=0
+    @CHECK_STATE_CHANGE
+    0;JMP
+(KEY_PRESSED)
+    @R1
+    M=1 // R1=1
+(CHECK_STATE_CHANGE) // if R0 != R1 then update screen
+    // check state
+    @R0
+    D=M // D=R0
+    @R1
+    D=D-M // D=R1-R0
+    @LOOP_KBD
+    D;JEQ
+
+    // prepare next
+    @R1
+    D=M // D=R1
+    @R0
+    M=D // R0=R1
+
+    // set color
+    @SET_WHITE
+    D;JEQ
+(SET_BLACK)
+    @color
+    M=-1 // color=-1
+    @LOOP_FILL
+    0;JMP
+(SET_WHITE)
+    @color
+    M=0 // color=0
+(LOOP_FILL)
+    // check end
+    @count
+    D=M // D=count
+    @number_of_pixel
+    D=M-D // D=number_of_pixel-count
+    @LOOP_KBD
+    0;JLT
+
+    // fill a pixel
+    @color
+    D=M // D=color
+    @position
+    A=M
+    M=D
+
+    // prepare next loop
+    @position
+    M=M+1 // position++
+    @count
+    M=M+1 // i++
+    @LOOP_FILL
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0 // R2=0
+    @i
+    M=1 // i=1
+(LOOP)
+    @i
+    D=M // D=i
+    @R1
+    D=D-M // D=i-R1
+    @END
+    D;JGT
+    @R0
+    D=M // D=R0
+    @R2
+    M=D+M // R2=R0+R2
+    @i
+    M=M+1 // i++
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
